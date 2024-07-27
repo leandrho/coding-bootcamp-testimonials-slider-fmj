7dfbca1000943e83e09414468e9807bc55bb6576
Leandro Ruiz <lean@localhost.localdomain>
2024-07-26 23:41:07 -0300

    Add index, styles and script

---
+++ script.js
@@ -0,0 +1,67 @@
+const texts = [
+    {
+        name: "Tanya Sinclair",
+        prof: "UX Engineer",
+        text: "“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”",
+        image: "./images/image-tanya.jpg"
+    },
+    {
+        name: "John Tarkpor",
+        prof: "Junior Front-end Developer",
+        text: "“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
+        image: "./images/image-john.jpg"
+    },
+];
+let current = 1;
+
+const doNext = () => {
+    current = current?0:1;
+    const obj = texts[current];
+
+    const nameElem = document.querySelector('.slider-name');
+    const textElem = document.querySelector('.slider-text');
+    const picElem = document.querySelector('.slider-pic');
+    const imgElem = document.querySelector('#slider-img');
+
+    nameElem.classList.add('fade-out');
+    textElem.classList.add('fade-out');
+    picElem.classList.add('fade-out');
+    imgElem.classList.add('fade-out');
+
+    setTimeout(() => {
+        nameElem.classList.remove('fade-out');
+        textElem.classList.remove('fade-out');
+        picElem.classList.remove('fade-out');
+        imgElem.classList.remove('fade-out');
+
+        nameElem.classList.add('fade-in');
+        nameElem.innerHTML=obj.name+" <span>"+obj.prof+"</span>"
+    
+        textElem.classList.add('fade-in');
+        textElem.innerHTML = obj.text;
+        
+        picElem.classList.add('fade-in');
+        imgElem.src = obj.image;
+    
+        setTimeout(() => {
+            nameElem.classList.remove('fade-in');
+            textElem.classList.remove('fade-in');
+            picElem.classList.remove('fade-in');
+            picElem.classList.remove('fade-in');
+        }, 1500);
+    }, 500);
+
+}
+
+const next = document.getElementById("next");
+next.addEventListener('click',(e)=>{
+    e.preventDefault();
+    doNext();
+});
+const prev = document.getElementById("prev");
+prev.addEventListener('click',(e)=>{
+    e.preventDefault();
+    doNext();
+});
+
+doNext();
